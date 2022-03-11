01aaea93a227115c50de7825c7485b5571b617eb
wcjameson <williamjameson90@gmail.com>
2022-03-11 13:41:28 -0800

    add delete and edit routes for Stylists controller

--- HairSalon/Controllers/StylistsController.cs.cs
+++ HairSalon/Controllers/StylistsController.cs.cs
@@ -38,5 +38,34 @@ namespace HairSalon.Controllers
       Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
       return View(thisStylist);
     }
+
+     public ActionResult Edit(int id)
+    {
+      var thisCategory = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+      return View(thisCategory);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Stylist stylist)
+    {
+      _db.Entry(stylist).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+      return View(thisStylist);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+      _db.Stylists.Remove(thisStylist);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
